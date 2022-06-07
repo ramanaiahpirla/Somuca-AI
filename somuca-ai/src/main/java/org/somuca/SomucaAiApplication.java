155c7fbd55afe1bb65a1c74e55e6d03101469c56
RamanaiahPirla <ramanaiah.237@gmail.com>
2022-06-07 18:08:46 +0530

    Initial Commit

---
+++ somuca-ai/src/main/java/org/somuca/SomucaAiApplication.java
@@ -0,0 +1,13 @@
+package org.somuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomucaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomucaAiApplication.class, args);
+	}
+
+}
